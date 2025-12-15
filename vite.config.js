6d6b13333dbcacf427035ccc672442035256944f
sila <silaksp26@gmail.com>
2025-12-15 19:50:32 +0700

    Update base path dan folder ke praktik-9

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/praktik-14-react/',
+  base: '/praktik-9-react/',
   plugins: [react()],
 })
